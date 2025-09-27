37926f50d7bbcb652421db3487495a08b88827a5
Scepty-SSB <168497715+Scepty-SSB@users.noreply.github.com>
2025-09-26 23:41:19 -0700

    Add comments and .gitignore

--- main.c
+++ main.c
@@ -12,12 +12,14 @@ int main (int argc, char ** argv) {
     // get position for first dirt piece
     char * tempStr = argv[1];
     int num = 0;
+    // this loop adds the ASCII values for every character in the string to get n integer that can later
+    // be used for the position
     for (int i = 0; strcmp(&tempStr[i], "\0") != 0; i++) {
         if (strcmp(&tempStr[i], " ") != 0){
             num += (int)tempStr[i];
         }
     }
-    // mod num by the number of columns in the terminal to ensure
-    // position will always be within the terminal
+    // mod num by the number of columns in the terminal to ensure position will always be
+    // within the terminal
     int position = num % w.ws_col;
 }
