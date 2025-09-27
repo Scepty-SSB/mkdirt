0eee8eded4a86547173f6132c96fd778ca5f36d4
Scepty-SSB <168497715+Scepty-SSB@users.noreply.github.com>
2025-09-26 23:40:16 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main (int argc, char ** argv) {
+    // get terminal size
+    struct winsize w;
+    ioctl(0, TIOCGWINSZ, &w);
+    printf("%i\n", w.ws_col);
+    // get position for first dirt piece
+    char * tempStr = argv[1];
+    int num = 0;
+    for (int i = 0; strcmp(&tempStr[i], "\0") != 0; i++) {
+        if (strcmp(&tempStr[i], " ") != 0){
+            num += (int)tempStr[i];
+        }
+    }
+    // mod num by the number of columns in the terminal to ensure
+    // position will always be within the terminal
+    int position = num % w.ws_col;
+}
