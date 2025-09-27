061c3c5cb1ffefa3cce0af7ea1ca3363b70be05f
Scepty-SSB <168497715+Scepty-SSB@users.noreply.github.com>
2025-09-27 01:03:55 -0700

    clean up main() and improve memory safety

--- main.c
+++ main.c
@@ -5,19 +5,21 @@
 
 
 int main (int argc, char ** argv) {
+    if (argc < 2) {
+        printf("Insufficient arguments\n");
+        return 0;
+    }
     // get terminal size
     struct winsize w;
     ioctl(0, TIOCGWINSZ, &w);
-    printf("%i\n", w.ws_col);
     // get position for first dirt piece
-    char * tempStr = argv[1];
+    char * tempStr = malloc(sizeof(argv[1]));
+    strcpy(tempStr, argv[1]);
     int position = 0;
     // this loop adds the ASCII values for every character in the string to get n integer that can later
     // be used for the position
     for (int i = 0; strcmp(&tempStr[i], "\0") != 0; i++) {
-        if (strcmp(&tempStr[i], " ") != 0){
-            position += (int)tempStr[i];
-        }
+       position += (int)tempStr[i];
     }
     // mod num by the number of columns in the terminal to ensure position will always be
     // within the terminal
