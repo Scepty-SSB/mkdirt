510e386a8dba52815df497ae4d40c3bf18a61579
Scepty-SSB <168497715+Scepty-SSB@users.noreply.github.com>
2025-10-05 19:26:36 -0700

    improve names and documentation

--- main.c
+++ main.c
@@ -11,41 +11,46 @@
 #define DIRT_CHAR '#'
 #define SET_TEXT_COLOR "\x1b[38;5;52m"
 
-/// Given the column number of the tip of a dirt pile and the size of the
-/// terminal, prints a pile of dirt.
-void printDirtPile(unsigned pos, unsigned rows, unsigned columns) {
-  char *output = vector_create();
-  vector_add(&output, columns);
+/// Prints a `4/5*rows` x `columns` pile of dirt with the tip at `tipColumn`.
+void printDirtPile(unsigned tipColumn, unsigned rows, unsigned columns) {
+  char *lineBuffer = vector_create();
+  vector_add(&lineBuffer, columns);
+  assert(vector_size(lineBuffer) == columns);
+
+  // fill lineBuffer with space characters
   for (vec_size_t outputPos = 0; outputPos < columns; outputPos++) {
-    assert(outputPos < vector_size(output));
-    output[outputPos] = ' ';
+    assert(outputPos < vector_size(lineBuffer));
+    lineBuffer[outputPos] = ' ';
   }
-  assert(pos < vector_size(output));
-  output[pos] = DIRT_CHAR;
+
+
+  assert(tipColumn < vector_size(lineBuffer));
+  lineBuffer[tipColumn] = DIRT_CHAR;
   char *changes = malloc((columns + 1) * sizeof(char));
   size_t changes_size = columns + 1;
-  strcpy(changes, output);
+  strcpy(changes, lineBuffer);
   for (unsigned i = 0; i < rows; i++) {
     if (i < floor((double)rows / 5)) {
       printf("\n");
     } else {
-      printf("%s\n", output);
+      printf("%s\n", lineBuffer);
       bool isBottomFull = true;
       unsigned checkIter = 0;
       while (isBottomFull && checkIter < columns) {
-        assert(checkIter < vector_size(output));
-        if (output[checkIter] != DIRT_CHAR) {
+        assert(checkIter < vector_size(lineBuffer));
+        if (lineBuffer[checkIter] != DIRT_CHAR) {
           isBottomFull = false;
         }
         checkIter++;
       }
       if (isBottomFull) {
+       vector_free(changes);
         return;
       }
       // set up next row
       for (unsigned vecPos = 0; vecPos < columns; vecPos++) {
-        assert(vecPos < vector_size(output));
-        if (output[vecPos] == DIRT_CHAR) {
+        assert(vecPos < vector_size(lineBuffer));
+        if (lineBuffer[vecPos] == DIRT_CHAR) {
           switch (rand() % 3) {
           case 1:
             assert(vecPos + 1 < changes_size);
@@ -94,7 +99,7 @@ void printDirtPile(unsigned pos, unsigned rows, unsigned columns) {
       assert(columns < changes_size);
       changes[columns] = '\0';
       // prepare to output next row
-      strcpy(output, changes);
+      strcpy(lineBuffer, changes);
     }
   }
   free(changes);
