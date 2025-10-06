910b70278144dfc575987b08f4cb82e98f44c00f
Scepty-SSB <168497715+Scepty-SSB@users.noreply.github.com>
2025-10-05 23:05:43 -0700

    Completely rewrite printDirtPile

--- main.c
+++ main.c
@@ -1,6 +1,4 @@
-#include "vec.h"
 #include <assert.h>
-#include <math.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -10,6 +8,20 @@
 
 #define DIRT_CHAR '#'
 #define SET_TEXT_COLOR "\x1b[38;5;52m"
+#define MIN(x, y)                                                                                                      \
+  ({                                                                                                                   \
+    const typeof(x) _x = (x);                                                                                          \
+    const typeof(y) _y = (y);                                                                                          \
+    (void)(&_x == &_y);                                                                                                \
+    _x < _y ? _x : _y;                                                                                                 \
+  })
+#define MAX(x, y)                                                                                                      \
+  ({                                                                                                                   \
+    const typeof(x) _x = (x);                                                                                          \
+    const typeof(y) _y = (y);                                                                                          \
+    (void)(&_x == &_y);                                                                                                \
+    _x > _y ? _x : _y;                                                                                                 \
+  })
 
 /// The composition of a line of output.
 typedef struct {
@@ -37,103 +49,38 @@ void printLine(Line l) {
   printCopies(l.dirtWidth, DIRT_CHAR);
   printCopies(l.trailingSpace, ' ');
   (void)putc('\n', stdout);
+  (void)fflush(stdout);
 }
 
-/// Prints a `4/5*rows` x `columns` pile of dirt with the tip at `tipColumn`.
+/// Returns the width of `l`.
+unsigned lineWidth(Line l) {
+  return l.leadingSpace + l.dirtWidth + l.trailingSpace;
+}
+
+/// Returns `l`, with its `leadingSpace` and `trailingSpace` reduced by `leadingShrinkage`
+/// and `trailingShrinkage` respectively, preserving its `lineWidth`.
+Line growDirt(Line l, unsigned leadingShrinkage, unsigned trailingShrinkage) {
+  unsigned s0 = MIN(l.leadingSpace, leadingShrinkage);
+  unsigned s1 = MIN(l.trailingSpace, trailingShrinkage);
+  l.leadingSpace -= s0;
+  l.trailingSpace -= s1;
+  l.dirtWidth += s0 + s1;
+  return l;
+}
+
+/// Prints a pile of dirt with the tip at `tipColumn` that is `columns` wide and `<= rows` tall.
 ///
 /// - Precondition: `tipColumn < columns`.
 void printDirtPile(unsigned tipColumn, unsigned rows, unsigned columns) {
   assert(tipColumn < columns);
-  char *lineBuffer = vector_create();
-
-  // fill lineBuffer with space characters
-  for (vec_size_t i = 0; i < columns; i++) {
-    vector_add(&lineBuffer, ' ');
-  }
-  vector_add(&lineBuffer, '\0');
-
-
-  lineBuffer[tipColumn] = DIRT_CHAR;
-  char *changes = vector_copy(lineBuffer);
-  size_t changes_size = columns + 1;
-  strcpy(changes, lineBuffer);
-
-
-  for (unsigned i = 0; i < rows; i++) {
-    if (i < floor((double)rows / 5)) {
-      printf("\n");
-    } else {
-      printf("%s\n", lineBuffer);
-      bool isBottomFull = true;
-      unsigned checkIter = 0;
-      while (isBottomFull && checkIter < columns) {
-        assert(checkIter < vector_size(lineBuffer));
-        if (lineBuffer[checkIter] != DIRT_CHAR) {
-          isBottomFull = false;
-        }
-        checkIter++;
-      }
-      if (isBottomFull) {
-        vector_free(changes);
-        return;
-      }
-      // set up next row
-      for (unsigned vecPos = 0; vecPos < columns; vecPos++) {
-        assert(vecPos < vector_size(lineBuffer));
-        if (lineBuffer[vecPos] == DIRT_CHAR) {
-          switch (rand() % 3) {
-          case 1:
-            assert(vecPos + 1 < changes_size);
-            changes[vecPos - 1] = DIRT_CHAR;
-            changes[vecPos + 1] = DIRT_CHAR;
-            if (rand() % rows > rows / i) {
-              changes[vecPos - 2] = DIRT_CHAR;
-            }
-            break;
-          case 2:
-            if (vecPos < columns) {
-              assert(vecPos + 1 < changes_size);
-              changes[vecPos + 1] = DIRT_CHAR;
-              if (rand() % rows > rows / i && vecPos < columns - 1) {
-                changes[vecPos - 2] = DIRT_CHAR;
-              }
-            }
-            assert(vecPos - 1 < changes_size);
-            changes[vecPos - 1] = DIRT_CHAR;
-            break;
-          default:
-            assert(vecPos - 1 < changes_size);
-            changes[vecPos - 1] = DIRT_CHAR;
-            unsigned consitentRand = (unsigned)rand();
-            if (consitentRand % rows > rows / i) {
-              changes[vecPos - 2] = DIRT_CHAR;
-            }
-            if (vecPos < columns) {
-              assert(vecPos + 1 < changes_size);
-              changes[vecPos + 1] = DIRT_CHAR;
-              if (consitentRand % rows > rows / i && vecPos < columns - 1) {
-                assert(vecPos + 2 < changes_size);
-                changes[vecPos + 2] = DIRT_CHAR;
-              }
-            }
-            break;
-          }
-        } else if (rand() % (10 * columns) == 1) {
-          assert(vecPos < changes_size);
-          changes[vecPos] = DIRT_CHAR;
-        }
-      }
-      // prevents a bug where one extra DIRT_CHAR was being printed
-      assert(changes_size > 0);
-      vector_pop(changes);
-      assert(columns < changes_size);
-      changes[columns] = '\0';
-      // prepare to output next row
-      strcpy(lineBuffer, changes);
+  Line currentLine = newLine(tipColumn, 1, columns - (1 + tipColumn));
+  for (unsigned i = 1; i <= rows; i++) {
+    printLine(currentLine);
+    currentLine = growDirt(currentLine, (unsigned)rand() % i, (unsigned)rand() % i);
+    if (currentLine.dirtWidth == lineWidth(currentLine)) {
+      return;
     }
   }
-  free(changes);
-  return;
 }
 
 typedef struct winsize winsize;
@@ -158,7 +105,7 @@ unsigned hash(char const *const s) {
 
 int main(int const argc, char const *const argv[]) {
   if (argc < 2) {
-    fprintf(stderr, "Insufficient arguments\n");
+    (void)fprintf(stderr, "Insufficient arguments\n");
     return -1;
   }
   // set output color on ANSI terminals
